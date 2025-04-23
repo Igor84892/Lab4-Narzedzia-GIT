34db05bee474738d8f27a24d7ba8ed99a6354f00
Igor Shybanov <talionigor@gmail.com>
2025-04-23 14:34:41 +0200

    Add --logs option to skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -2,4 +2,12 @@
 
 if [[ "$1" == "--date" ]]; then
   date
+
+elif [[ "$1" == "--logs" ]]; then
+  count=${2:-100}
+  for ((i=1; i<=count; i++)); do
+    filename="log$i.txt"
+    echo "$filename - $(basename $0) - $(date)" > $filename
+  done
 fi
+
