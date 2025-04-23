c620c0c350006a1d7c96e92e6af24233f2858a0f
Igor Shybanov <talionigor@gmail.com>
2025-04-23 14:44:15 +0200

    Add short flags (-h, -d, -l)

--- skrypt.sh
+++ skrypt.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-if [[ "$1" == "--help" ]]; then
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
   echo "Available options:"
-  echo "--date           show current date"
-  echo "--logs [n]       create n log files (default 100)"
-  echo "--help           show this help"
+  echo "--date / -d      show current date"
+  echo "--logs [n] / -l  create n log files (default 100)"
+  echo "--help / -h      show this help"
 
-elif [[ "$1" == "--date" ]]; then
+elif [[ "$1" == "--date" || "$1" == "-d" ]]; then
   date
 
-elif [[ "$1" == "--logs" ]]; then
+elif [[ "$1" == "--logs" || "$1" == "-l" ]]; then
   count=${2:-100}
   for ((i=1; i<=count; i++)); do
     filename="log$i.txt"
